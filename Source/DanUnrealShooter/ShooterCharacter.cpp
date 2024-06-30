d1c1d59e861c356bb7e0c765e0ff040a853b05b5
Daniel <danielimbert90@gmail.com>
2024-06-30 22:15:47 +1000

    C++ char movement
    added movement to shooter.cpp

--- Source/DanUnrealShooter/ShooterCharacter.cpp
+++ Source/DanUnrealShooter/ShooterCharacter.cpp
@@ -2,12 +2,46 @@
 
 
 #include "ShooterCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+ 
+
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	 // Instantiating your class Components
+ 
+     SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+ 
+     CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+ 
+     //Set the location and rotation of the Character Mesh Transform
+ 
+     GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FQuat(FRotator(0.0f, -90.0f, 0.0f)));
+ 
+     // Attaching your class Components to the default character's Skeletal Mesh Component.
+ 
+     SpringArmComp->SetupAttachment(GetMesh());
+ 
+     CameraComp->SetupAttachment(SpringArmComp,USpringArmComponent::SocketName);
+ 
+     //Setting class variables of the spring arm
+ 
+     SpringArmComp->bUsePawnControlRotation = true;
+ 
+     //Setting class variables of the Character movement component
+ 
+     GetCharacterMovement()->bOrientRotationToMovement = true;
+ 
+     GetCharacterMovement()->bUseControllerDesiredRotation = true;
+ 
+     GetCharacterMovement()->bIgnoreBaseRotation = true;
+ 
 
 }
 
