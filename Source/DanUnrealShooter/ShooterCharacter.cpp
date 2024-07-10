4d0bc441f68055fdc44fe2977b30fb199d333cba
Daniel <danielimbert90@gmail.com>
2024-07-10 19:04:48 +1000

    Disable Capsule Collisions on Death

--- Source/DanUnrealShooter/ShooterCharacter.cpp
+++ Source/DanUnrealShooter/ShooterCharacter.cpp
@@ -3,6 +3,7 @@
 #include "EnhancedInputSubsystems.h"
 #include "GameFramework/Controller.h"
 #include "Gun.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -85,6 +86,13 @@ float AShooterCharacter::TakeDamage(float DamageAmount, struct FDamageEvent cons
 	DamageToApply = FMath::Min(Health, DamageToApply);
 	Health -= DamageToApply;
 	UE_LOG(LogTemp, Warning, TEXT("Health left %f"), Health);
+
+    if (IsDead())
+	{
+		DetachFromControllerPendingDestroy();
+		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+
 
 	return DamageToApply;
 }
