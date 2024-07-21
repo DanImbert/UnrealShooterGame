000229c3a16be54e35b72d830752fa975bf9673b
Daniel <danielimbert90@gmail.com>
2024-07-21 13:50:19 +1000

    Create HUD - User Interface

--- Source/DanUnrealShooter/ShooterPlayerController.cpp
+++ Source/DanUnrealShooter/ShooterPlayerController.cpp
@@ -7,9 +7,23 @@
 
 
 
+void AShooterPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    HUD = CreateWidget(this, HUDClass);
+    if (HUD != nullptr)
+    {
+        HUD->AddToViewport();
+    }
+}
+
+
 void AShooterPlayerController::GameHasEnded(class AActor *EndGameFocus, bool bIsWinner) 
 {
     Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+    HUD->RemoveFromViewport();
 
      if (bIsWinner)
     {
--- Source/DanUnrealShooter/ShooterPlayerController.h
+++ Source/DanUnrealShooter/ShooterPlayerController.h
@@ -17,12 +17,19 @@ class DANUNREALSHOOTER_API AShooterPlayerController : public APlayerController
 public:
 	virtual void GameHasEnded(class AActor *EndGameFocus = nullptr, bool bIsWinner = false) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
 private:
+
+    UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> HUDClass;
 
     UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> WinScreenClass;
 
-	
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> LoseScreenClass;
 
@@ -30,6 +37,9 @@ private:
 	float RestartDelay = 5;
 
 	FTimerHandle RestartTimer;
+
+    UPROPERTY(EditAnywhere)
+	UUserWidget *HUD;
 
 	
 };
